54718cb202bf83b15b8028b962697732d90e6843
belino93 <abelmadrid93@gmail.com>
2022-10-03 17:04:51 +0200

    Reto 2 y 2.1 terminados

---
+++ main.js
@@ -0,0 +1,16 @@
+/* 
+Declara un String que contenga tu nombre, después muestra un mensaje de
+bienvenida por consola. Por ejemplo: si introduzco “Fernando”, me aparezca
+“Bienvenido Fernando”. 
+*/
+
+let nombre = 'Abel';
+console.log('Bienvenido ' + nombre)
+
+/* 
+Modifica la aplicación anterior, para que nos pida el nombre que queremos
+introducir (recuerda usar prompt). 
+*/
+
+let nombre2 = prompt('Introduce tu nombre');
+console.log('Bienvenido ' + nombre2);
